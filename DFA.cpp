b239b4d664c8dc4dcefcd9d65acebccff94c2e8c
Aaditya Nepal <100606207+nepalaaditya2002@users.noreply.github.com>
2022-05-09 18:04:53 +0200

    back to ssc

--- DFA.cpp
+++ DFA.cpp
@@ -1,14 +1,20 @@
 //
 // Created by aaditya on 23.04.22.
 //
-
 #include "DFA.h"
-#include "iomanip"
-DFA::DFA(string file) {
-    ifstream input(file);
+#include <iostream>
+#include <fstream>
+#include "json.hpp"
+
+using namespace std;
+
+using json = nlohmann::json;
+
+DFA::DFA(string p){
+    filename = p;
+    ifstream input(filename);
     input >> j;
-    input.close();
 }
-void DFA::print() {
+void DFA::print(){
     cout << setw(4) << j << endl;
 }
--- DFA.h
+++ DFA.h
@@ -4,20 +4,25 @@
 
 #ifndef SUBSET_CONSTRUCTION_DFA_H
 #define SUBSET_CONSTRUCTION_DFA_H
-#include "iostream"
-#include "fstream"
-#include "string"
+#include <string>
+#include "iomanip"
+#include <iostream>
 #include "json.hpp"
+#include <set>
+#include <map>
 
 using json = nlohmann::json;
 using namespace std;
-
 class DFA {
 private:
+    string filename;
     json j;
+    json dfa1;
+    json dfa2;
+    bool doorsnede;
 public:
 
-    DFA(string file);
+    DFA(string p);
 
     void print();
 
