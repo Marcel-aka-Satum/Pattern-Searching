0e272cc871285e631d05e49783e7f08b84c6b585
elias2060 <77207412+elias2060@users.noreply.github.com>
2022-06-07 21:50:55 +0200

    Update outputTest.h

--- test/outputTest.h
+++ test/outputTest.h
@@ -5,7 +5,7 @@
 #ifndef PATTERN_SEARCHING_OUTPUTTEST_H
 #define PATTERN_SEARCHING_OUTPUTTEST_H
 #include "../gtest/include/gtest/gtest.h"
-#include "../helper.h"
+#include "../utils.h"
 #include "../RE.h"
 #include "../ENFA.h"
 #include "../DFA.h"
@@ -14,26 +14,26 @@
 class outputTest : public ::testing::Test{};
 
 TEST_F(outputTest, jsonOutput){
-    ASSERT_TRUE(DirectoryExists("../outputs"));
+    ASSERT_TRUE(DirectoryExists("testOutput"));
     RE re1("Aaditya", 'e');
     ENFA enfa = re1.toENFA();
     DFA dfa = enfa.toDFA();
     RE re2 = dfa.toRE();
     string regex = re2.getRegex();
 
-    ASSERT_TRUE(FileExists("../outputs/dfa-output.json"));
-    ASSERT_TRUE(FileExists("../outputs/enfa-output.json"));
-    ASSERT_TRUE(FileExists("../outputs/mindfa.json"));
-    ASSERT_TRUE(FileExists("../outputs/regex.txt"));
+    ASSERT_TRUE(FileExists("testOutput/dfa-output.json"));
+    ASSERT_TRUE(FileExists("testOutput/enfa-output.json"));
+    ASSERT_TRUE(FileExists("testOutput/mindfa.json"));
+    ASSERT_TRUE(FileExists("testOutput/regex.txt"));
     string ss = "";
-    ifstream file("../outputs/regex.txt");
+    ifstream file("testOutput/regex.txt");
     file >> ss;
     file.close();
 
 
-    EXPECT_TRUE(FileCompare("../outputs/dfa-output.json","../dfa-output.json"));
-    EXPECT_TRUE(FileCompare("../outputs/enfa-output.json","../enfa-output.json"));
-    EXPECT_TRUE(FileCompare("../outputs/mindfa.json","../tfa.json"));
+    EXPECT_TRUE(FileCompare("testOutput/dfa-output.json","testOutput/dfa-output.json"));
+    EXPECT_TRUE(FileCompare("testOutput/enfa-output.json","testOutput/enfa-output.json"));
+    EXPECT_TRUE(FileCompare("testOutput/mindfa.json","testOutput/mindfa.json"));
     ASSERT_EQ(ss,regex);
 
 }
